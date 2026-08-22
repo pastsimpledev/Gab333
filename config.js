1d48772dbf4a3a187d92f3cee79c7bc26956d57c
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-08-22 11:11:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,78 +1,101 @@
-import { watchFile, unwatchFile } from 'fs'
-import { fileURLToPath, pathToFileURL } from 'url'
-import chalk from 'chalk'
-import fs from 'fs'
-import * as cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone'
-import NodeCache from 'node-cache'
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import fs from 'fs';
+import { fileURLToPath, pathToFileURL } from 'url';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
-const pkg = JSON.parse(await fs.promises.readFile(new URL('./package.json', import.meta.url), 'utf-8'))
-const moduleCache = new NodeCache({ stdTTL: 300 });
+global.botnumber = '';
+global.confirmCode = '';
+global.nomebot = '6lokka';
+global.packname = '6lokka';
+global.author = '𝐌𝐝';
+global.vs = '11';
+global.collab = 'Demo';
+global.wm = global.nomebot;
+global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
-	
-global.gab = ['393455383567',]
+
 global.owner = [
-  ['9779828891521', 'gnegne', true],
-  ['x', 'x', true],
-  ['x', 'x', true],
-  ['x', 'x', true],
-]
+  ['3455383567', 'night', true],
+  ['xxxxxxxxxx'], // Numero, Nome, True (attenzione alla sintassi deve essere identico a sopra!)
+  ['xxxxxxxxxx'],
+  ['xxxxxxxxxx'],
+  ['xxxxxxxxxx'],
+  ['xxxxxxxxxx']
+];
 
 
-global.nomepack = '333'
-global.nomebot = ' ꙰ 𝟥𝟥𝟥 𝔹𝕆𝕋  ꙰'
-global.wm = '333'
-global.autore = 'gab'
-global.dev = 'lucifero'
-global.testobot = `333`
-global.versione = pkg.version
-global.errore = '⚠️ *Errore inatteso!* Usa il comando `.ticket` per avvisare gli owner.'
+global.mods = ['xxxxxxxxxx'];
+global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx'];
+
+global.multiplier = 69;
+global.maxwarn = '4';
+
+global.openai_key = 'sk-0';
+global.openai_org_id = 'org-3';
+
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())];
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
+global.itsrose = ['4b146102c4d500809da9d1ff'];
+
+global.APIs = {
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://api.zahwazein.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  fgmods: 'https://api-fgmods.ddns.net',
+  botcahx: 'https://api.botcahx.biz.id',
+  ibeng: 'https://api.ibeng.tech/docs',
+  rose: 'https://api.itsrose.site',
+  popcat: 'https://api.popcat.xyz',
+  xcoders: 'https://api-xcoders.site'
+};
+
+global.APIKeys = {
+  'https://api.xteam.xyz': global.keysxteam,
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': global.keysneoxr,
+  'https://violetics.pw': 'beta',
+  'https://api.zahwazein.xyz': global.keysxxx,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://api.botcahx.biz.id': 'Admin',
+  'https://api.ibeng.tech/docs': 'tamvan',
+  'https://api.itsrose.site': 'Rs-Zeltoria',
+  'https://api-xcoders.site': 'Frieren',
+  openrouter: 'varebot'
+};
+
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+
+    const results = Object.keys(emotttt).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string));
+    if (!results.length) return '';
+    return emotttt[results[0][0]];
+  }
+};
 
 
-global.repobot = 'https://github.com/GabWT333/Gab333'
-global.canale = 'https://whatsapp.com/channel/0029VauhQviCsU9Ibrwlkb0h'
-global.gruppo = 'https://chat.whatsapp.com/L7Hz1iQpctTDfOoU4LARWL?mode=gi_t' 
 
-
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment
-
-
-global.APIKeys = { 
-    spotifyclientid: '333',
-    spotifysecret: '333',
-    browserless: '333',
-    screenshotone: '333',
-    screenshotone_default: '333',
-    tmdb: '333',
-    gemini:'333',
-    ocrspace: '333',
-    assemblyai: '333',
-    google: '333',
-    googlex: '333',
-    googleCX: '333',
-    genius: '333',
-    unsplash: '333',
-    removebg: 'FEx4CYmYN1QRQWD1mbZp87jV',
-    openrouter: '333',
-    lastfm: '36f859a1fc4121e7f0e931806507d5f9',
-}
-
-
-let filePath = fileURLToPath(import.meta.url)
-let fileUrl = pathToFileURL(filePath).href
-const reloadConfig = async () => {
-  const cached = moduleCache.get(fileUrl);
-  if (cached) return cached;
-  unwatchFile(filePath)
-  console.log(chalk.bgHex('#ff0000')(chalk.white.bold("File: 'config.js' Aggiornato")))
-  const module = await import(`${fileUrl}?update=${Date.now()}`)
-  moduleCache.set(fileUrl, module, { ttl: 300 });
-  return module;
-}
-watchFile(filePath, reloadConfig)
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright("🔄 Config aggiornato: 'config.js'"));
+  import(`${pathToFileURL(file).href}?update=${Date.now()}`);
+});
